9dfc1819d6b602329011dc0f17fb6fccadea4e0a
Lars De Roover <67840433+DR-Lars@users.noreply.github.com>
2024-06-11 12:42:49 +0200

    chore: Place navbar in layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script>
-  import Navbar from "./components/navbar.svelte";
 </script>
 
-<Navbar />
 <div class="px-5 py-3">
   <h1>Welcome to SvelteKit</h1>
   <a href="/appointment">Make an appointment</a>
--- src/routes/appointment/+page.svelte
+++ src/routes/appointment/+page.svelte
@@ -1,9 +1,7 @@
 <script>
-  import Navbar from "../components/navbar.svelte";
   import Appointment from "../components/appointment.svelte";
 </script>
 
-<Navbar />
 <main class="grid grid-cols-1 gap-x-6 gap-y-3 px-5 py-3 sm:grid-cols-2">
   <section class="flex flex-col space-y-4">
     <h1 class="text-xl font-semibold">Make an Appointment</h1>
