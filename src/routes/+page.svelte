65525319f69d2465cf8dda333a162048e6ef2af9
Lars De Roover <67840433+DR-Lars@users.noreply.github.com>
2024-06-12 02:06:45 +0200

    chore: Add blue, green, and white dividers with SVG shapes to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,35 @@
 <script>
+  import DividerBlue from "./components/dividers/divider-blue.svelte";
+  import DividerGreen from "./components/dividers/divider-green.svelte";
+  import DividerWhite from "./components/dividers/divider-white.svelte";
 </script>
 
-<div class="px-5 py-3">
-  <h1>Welcome to SvelteKit</h1>
-  <a href="/appointment">Make an appointment</a>
+<div id="homescreen">
+  <section class="relative flex flex-col items-center">
+    <DividerBlue />
+  </section>
+  <section class="relative flex flex-col items-center">
+    <DividerGreen />
+  </section>
+  <section class="relative flex flex-col items-center">
+    <DividerWhite />
+  </section>
+  <section class="relative flex flex-col items-center">
+  </section>
 </div>
+
+<style>
+  #homescreen section {
+    min-height: 100vh;
+  }
+
+  #homescreen section:nth-child(3n-1) {
+    background: linear-gradient(#22d3ee 0%, #5eead4 100%);
+    text-color: #6ee7b7;
+  }
+
+  #homescreen section:nth-child(3n) {
+    background: linear-gradient(#34d399 0%, #4ade80 100%);
+    text-color: #6ee7b7;
+  }
+</style>
